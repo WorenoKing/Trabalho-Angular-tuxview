d3c60e9a95d6f129e0b83ce2c8b1140d7855a3bc
Pedro Henrique Cursino Cabral <97694134+WorenoKing@users.noreply.github.com>
2022-10-28 10:48:54 -0300

    Exercício 5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FormularioComponent } from './formulario/formulario.component';
+import { FotoComponent } from './foto/foto.component';
+import { InicioComponent } from './inicio/inicio.component';
+
+const routes: Routes = [
+  { path: '', component: InicioComponent},
+  { path: 'inicio', component: InicioComponent },
+  { path: 'fotos', component: FotoComponent },
+  { path: 'formulario', component: FormularioComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FotoComponent } from './foto/foto.component';
 import { Header } from './head/head.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FotoComponent,
-    Header
+    Header,
+    InicioComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
